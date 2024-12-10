34b36c562d35b8a06af822b9075b6a1633b69954
VIKRAM SINGH <145076898+vikramsingh9219251196@users.noreply.github.com>
2024-12-10 13:31:58 +0530

    Update auth.service.jsx

--- cdl-onedrive-realty-frontend/src/services/auth.service.jsx
+++ cdl-onedrive-realty-frontend/src/services/auth.service.jsx
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "https://driving-management-system.onrender.com/";
+const API_URL = "https://driving-management-system.onrender.com";
 
 class AuthService {
   async register(credentials) {
