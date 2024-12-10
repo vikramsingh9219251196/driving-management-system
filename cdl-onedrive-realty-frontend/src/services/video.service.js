d9c1e7a9a23ec4a52736d2f066a1cfadac900b14
VIKRAM SINGH <145076898+vikramsingh9219251196@users.noreply.github.com>
2024-12-10 13:17:36 +0530

    Update video.service.js

--- cdl-onedrive-realty-frontend/src/services/video.service.js
+++ cdl-onedrive-realty-frontend/src/services/video.service.js
@@ -1,7 +1,7 @@
 // services/video.service.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://driving-management-system.onrender.com/';
 
 const videoService = {
   uploadCourse: async (formData, config = {}) => {
